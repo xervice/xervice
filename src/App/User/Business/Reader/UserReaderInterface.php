27bcb723aa221e653ebcd58c338f5d0b80dca217
Mike Bertram <contact@mibexx.de>
2018-07-31 14:44:21 +0200

    declare strict types

--- src/App/User/Business/Reader/UserReaderInterface.php
+++ src/App/User/Business/Reader/UserReaderInterface.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace App\User\Business\Reader;
 
 use DataProvider\UserDataProvider;
--- src/App/User/Business/Writer/UserWriterInterface.php
+++ src/App/User/Business/Writer/UserWriterInterface.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace App\User\Business\Writer;
 
 use DataProvider\UserDataProvider;
