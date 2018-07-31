8672f3d779366157f7454c1ed5bd63fc82c6323f
Mike Bertram <contact@mibexx.de>
2018-07-31 13:08:20 +0200

    * CI settings

---
+++ config/config_scrutinizer.php
@@ -0,0 +1,25 @@
+<?php
+
+
+use Xervice\Database\DatabaseConfig;
+use Xervice\RabbitMQ\RabbitMQConfig;
+use Xervice\Redis\RedisConfig;
+
+// RabbitMQ
+$config[RabbitMQConfig::CONNECTION_HOST] = '127.0.0.1';
+$config[RabbitMQConfig::CONNECTION_PORT] = 5672;
+$config[RabbitMQConfig::CONNECTION_USERNAME] = 'guest';
+$config[RabbitMQConfig::CONNECTION_PASSWORD] = 'guest';
+$config[RabbitMQConfig::CONNECTION_VIRTUALHOST] = '/';
+
+// Redis
+$config[RedisConfig::REDIS_HOST] = '127.0.0.1';
+$config[RedisConfig::REDIS_PORT] = 6379;
+
+
+// Database
+$config[DatabaseConfig::PROPEL_CONF_HOST] = '127.0.0.1';
+$config[DatabaseConfig::PROPEL_CONF_PORT] = '5432';
+$config[DatabaseConfig::PROPEL_CONF_DBNAME] = 'scrutinizer';
+$config[DatabaseConfig::PROPEL_CONF_USER] = 'scrutinizer';
+$config[DatabaseConfig::PROPEL_CONF_PASSWORD] = 'scrutinizer';
